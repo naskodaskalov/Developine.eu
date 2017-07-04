dce1eb572672aafccecdf17a5fa8cc0453fc8d02
Nasko Daskalov <nasko.daskalov@abv.bg>
2017-07-04 11:14:21 +0300

    Some design changes

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -10,18 +10,24 @@ class Portfolio extends Component {
             <ProjectList
               url='http://cataleatradingreports.herokuapp.com'
               projectName='Catalea Research Engine'
+              progress='Under Construction'
+              projectDescription='Catalea offers you the advantage of reading SEC filings online with easier-to-read text, faster navigation within a filing.'
               imageLink={require('../Images/catalea.png')} />
           </div>
           <div className='col-md-4'>
             <ProjectList
               url='http://obiavime.developine.eu'
               projectName='Obiavi.Me'
+              progress='Finished'
+              projectDescription='A project made during a course in Software University. Used technologies: HTML, PHP, MySQL Database'
               imageLink={require('../Images/obiavime.png')} />
           </div>
           <div className='col-md-4'>
             <ProjectList
               url='http://ganyoexpress.developine.eu'
               projectName='GanyoExpress'
+              progress='Finished'
+              projectDescription='A project made during a course in Software University. Used technologies: ReactJS, Bootstrap, Kinvey Database'
               imageLink={require('../Images/ganyoexpress.png')} />
           </div>
         </div>
--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -3,9 +3,14 @@ import './ProjectList.css'
 
 class ProjectList extends Component {
   render () {
+    let progressStyle = 'badge '
+    if (this.props.progress === 'Finished') {
+      progressStyle += 'badge-success'
+    } else if (this.props.progress === 'Under Construction') {
+      progressStyle += 'badge-info'
+    }
     return (
       <div className='wrapper'>
-
         <div className='card'>
           <div className='view overlay hm-white-slight'>
             <img src={this.props.imageLink} className='img-fluid' alt={this.props.imageAlt} />
@@ -14,10 +19,12 @@ class ProjectList extends Component {
             </a>
           </div>
           <div className='card-block'>
-            <h4 className='card-title'>{this.props.projectName}</h4>
+            <div className='title-progress'>
+              <h4 className='card-title'>{this.props.projectName}</h4><span className={progressStyle}>{this.props.progress}</span>
+            </div>
             <hr />
-            <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href={this.props.url} className='black-text d-flex flex-row-reverse'><h5 className='btn_read_more'>Read more <i className='fa fa-chevron-right' /></h5></a>
+            <p className='card-text'>{this.props.projectDescription}}</p>
+            <a href={this.props.url} className='black-text d-flex flex-row-reverse'><h5 className='btn_read_more'>Browse it <i className='fa fa-chevron-right' /></h5></a>
           </div>
         </div>
 
