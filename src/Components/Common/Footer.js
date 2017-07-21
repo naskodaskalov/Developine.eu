9c4a94d817d3349cb5596b8340cdcde1ba4c2d7b
Nasko Daskalov <nasko.daskalov@abv.bg>
2017-07-21 19:30:30 +0300

    some changes

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -13,18 +13,8 @@ class Footer extends Component {
               <li><a href='https://www.facebook.com/nasko.daskalov'><i className='fa fa-facebook-square' />/nasko.daskalov</a></li>
               <li><a href='https://github.com/naskodaskalov'><i className='fa fa-github-square' />/naskodaskalov</a></li>
               <li><a href='https://www.linkedin.com/in/nasko-daskalov-a7731a117/'><i className='fa fa-linkedin-square' />/nasko-daskalov-a7731a117</a></li>
-              {/* <li><a href='https://www.upwork.com/o/profiles/users/_~018374ab3cba0fc563/'><i className='fa fa-external-link' /> Upwork</a></li> */}
             </ul>
           </div>
-
-          {/* <div className='col-md-3 offset-md-1'>
-            <h5 className='title'>Used technologies</h5>
-            <ul>
-              <li>NodeJS</li>
-              <li>ReactJS</li>
-              <li>Bootstrap</li>
-            </ul>
-          </div> */}
 
           <div className='col-md-3 offset-md-1'>
             <h5 className='title'>Last 3 Projects</h5>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -17,7 +17,7 @@ class HomePage extends Component {
             <h1><img src={require('../Images/nd.jpg')} alt='nasko daskalov image' /></h1>
           </div>
           <div className='admin-preview'>
-            I am <strong>Nasko Daskalov</strong>, a <strong>freelance Web Developer</strong>, based in Sofia, Bulgaria. +1 year experience in developing with <strong>Javascript libraries like NodeJS and ReactJS</strong>. Also <strong>good knowledge of WordPress development</strong> and <strong>some knowledge of C# development and MSSQL Databases</strong>.
+            <h1 className='fakeHOne'>I am <strong>Nasko Daskalov</strong>, a <strong>freelance Web Developer</strong>, based in Sofia, Bulgaria. +1 year experience in developing with <strong>Javascript libraries like NodeJS and ReactJS</strong>. Also <strong>good knowledge of WordPress development</strong> and <strong>some knowledge of C# development and MSSQL Databases</strong>.</h1>
           </div>
         </div>
         {/*
